3a38ede2a02e86c6d735595e626c6433f909b2d6
Richard Kellnberger <Richard1.Kellnberger@web.de>
2024-02-13 17:38:48 +0100

    CY profile

--- assets/python/fluidx3d/eval/models/CY.py
+++ assets/python/fluidx3d/eval/models/CY.py
@@ -11,7 +11,7 @@ Created on Wed Feb  7 14:25:38 2024
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import cumulative_trapezoid, trapezoid
-from scipy.special import lambertw
+from scipy.optimize import root_scalar  # type: ignore
 
 from fluidx3d.eval.style import cm
 
@@ -27,39 +27,36 @@ class CY:
         self.a1 = a1
         self.a2 = a2
 
-    def prepareVelocityProfile(self, R, G, j):  # TODO
-        c = 2**j * self.eta_s / (-G)
-        d = 2 * np.sqrt(self.epsilon) * self.lambda_p
+    def prepareVelocityProfile(self, R, G, j):  # TODO check j
+        stepSize = 1e-6
 
-        guesses = np.arange(0, R / c * (1 + 1e-6), R / c * 1e-6)
+        def stressBalance(gd):
+            return self.eta(gd) * gd + G / 2**j * R
 
-        def r(gd):
-            return c * gd - self.eta_p / self.eta_s * c / d * np.sqrt(lambertw(d**2 * gd**2).real)
+        sol = root_scalar(
+            stressBalance,
+            method="toms748",
+            bracket=[-G / 2**j * R / self.eta_s, -G / 2**j * R / self.eta_p],
+        )
 
-        rguesses = r(guesses)
-        idx = np.argmin(np.abs(rguesses - R))
-        if rguesses[idx] < R:
-            idx += 1
+        if not sol.converged:
+            print(f"{sol=}")
+            raise Exception("Could not find alpha2")
 
-        guessesFine = np.arange(
-            0, guesses[idx] * (1 + 3e-6), guesses[idx] * 1e-6
-        )  # The three is due tue offsets needing it
-        rguessesFine = r(guessesFine)
+        edgeGD = sol.root
 
-        rVelocity = 0.5 * (rguessesFine[:-1] + rguessesFine[1:])
-        velocity = cumulative_trapezoid(guessesFine, rguessesFine)
+        gds = np.arange(0, 1 + stepSize, stepSize) * edgeGD
+        rs = -2 / G * self.eta(gds) * gds
 
         def getInterpolGD():
             def interpol(r):  # Can only handle scalar r
-                idx = np.argmin(np.abs(rguessesFine - r))
-                if rguessesFine[idx] > r:
+                idx = np.argmin(np.abs(rs - r))
+                if (rs[idx] > r and idx != 0) or idx == rs.shape[0] - 1:  # Make one sided instead
                     idx -= 1
-                elif rguessesFine[idx] == r:
-                    return guessesFine[idx]
-                m = (guessesFine[idx + 1] - guessesFine[idx]) / (
-                    rguessesFine[idx + 1] - rguessesFine[idx]
-                )
-                return guessesFine[idx] + m * (r - rguessesFine[idx])
+                elif rs[idx] == r:
+                    return gds[idx]
+                m = (gds[idx + 1] - gds[idx]) / (rs[idx + 1] - rs[idx])
+                return gds[idx] + m * (r - rs[idx])
 
             def interpolLoop(r):  # Slow Python loop
                 out = []
@@ -77,50 +74,18 @@ class CY:
 
         self.gd = getInterpolGD()
 
+        rv = 0.5 * (rs[:-1] + rs[1:])
+        velocity = cumulative_trapezoid(gds, rs)
+
         def getInterpol(offset=0):
             def interpol(r):  # Can only handle scalar r
-                idx = np.argmin(np.abs(rVelocity - r))
-                if idx == 0:
-                    m = (velocity[idx + 1] - velocity[idx]) / (rVelocity[idx + 1] - rVelocity[idx])
-                    return velocity[idx] - m * (r - rVelocity[idx]) - offset
-                if rVelocity[idx] > r:
+                idx = np.argmin(np.abs(rv - r))
+                if (rv[idx] > r and idx != 0) or idx == rv.shape[0] - 1:  # Make one sided instead
                     idx -= 1
-                elif rVelocity[idx] == r:
+                elif rv[idx] == r:
                     return velocity[idx] - offset
-                m = (velocity[idx + 1] - velocity[idx]) / (rVelocity[idx + 1] - rVelocity[idx])
-                return velocity[idx] + m * (r - rVelocity[idx]) - offset
-
-            def interpolBroadcast(r):  # Nice, but needs ram
-                idx = np.argmin(
-                    np.abs(np.repeat([rVelocity], r.size, axis=0) - np.expand_dims(r, axis=1)),
-                    axis=1,
-                )
-                idx -= rVelocity[idx] > r
-
-                out = (
-                    (idx == -1)
-                    * (
-                        velocity[idx]
-                        - (
-                            (velocity[idx + 2] - velocity[idx + 1])
-                            / (rVelocity[idx + 2] - rVelocity[idx + 1])
-                        )
-                        * (r - rVelocity[idx])
-                        - offset
-                    )
-                    + (rVelocity[idx] == r) * (velocity[idx] - offset)
-                    + (1 - (idx == -1) * (rVelocity[idx] == r))
-                    * (
-                        velocity[idx]
-                        + (
-                            (velocity[idx + 1] - velocity[idx])
-                            / (rVelocity[idx + 1] - rVelocity[idx])
-                        )
-                        * (r - rVelocity[idx])
-                        - offset
-                    )
-                )
-                return out
+                m = (velocity[idx + 1] - velocity[idx]) / (rv[idx + 1] - rv[idx])
+                return velocity[idx] + m * (r - rv[idx]) - offset
 
             def interpolLoop(r):  # Slow Python loop
                 out = []
@@ -145,13 +110,14 @@ class CY:
             2
             * np.pi**j
             * trapezoid(
-                np.concatenate(([0], rVelocity[:cutoff], [R])) ** j
+                np.concatenate(([0], rv[:cutoff], [R])) ** j
                 * np.concatenate(([self.u(0)], velocity[:cutoff] - offset, [0])),
-                np.concatenate(([0], rVelocity[:cutoff], [R])),
+                np.concatenate(([0], rv[:cutoff], [R])),
             )
         )
 
     def eta(self, gd):
+        gd = np.abs(gd)
         return (
             self.eta_p / (1 + (self.lambda_p * gd) ** self.a1) ** (self.a2 / self.a1) + self.eta_s
         )
@@ -189,13 +155,6 @@ class CY:
         plt.legend()
         plt.show()
 
-        plt.figure(figsize=(15.5 * cm, 15.5 / 2 * cm))
-        plt.plot(r * 1e6, self.N_1(gd), label=r"$N_1$")
-        plt.xlabel(r"$r/\unit{\micro\meter}$")
-        plt.ylabel(r"$N_1/\unit{\pascal}$")
-        plt.legend()
-        plt.show()
-
         rho = 1e3
         Re = rho * u * np.max(r) / eta
 
@@ -203,6 +162,13 @@ class CY:
         plt.plot(r * 1e6, Re, label=r"$Re$")
         plt.xlabel(r"$r/\unit{\micro\meter}$")
         plt.ylabel(r"$Re$")
+        plt.legend()
+        plt.show()
+
+        plt.figure(figsize=(15.5 * cm, 15.5 / 2 * cm))
+        plt.plot(r * 1e6, eta * gd, label=r"$\sigma$")
+        plt.xlabel(r"$r/\unit{\micro\meter}$")
+        plt.ylabel(r"$\sigma/\unit{\pascal}$")
         plt.legend()
         plt.show()
 
